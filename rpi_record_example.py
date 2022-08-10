f32bc6a6335c175f9f2c92efb323cf1b3d40e2bb
Yun Min Woo <77202633+ymw0407@users.noreply.github.com>
2022-08-11 02:46:52 +0900

    Update rpi_record_example.py

--- rpi_record_example.py
+++ rpi_record_example.py
@@ -1,5 +1,6 @@
 from picamera import PiCamera
 from time import sleep
+import os
 
 camera = PiCamera()
 #camera.rotation = 180
@@ -9,3 +10,8 @@ camera.start_recording('/home/pi/Desktop/video.h264')
 sleep(5)
 camera.stop_recording()
 camera.stop_preview()
+
+if os.path.isfile("video.mp4"):
+  os.remove("video.mp4")
+  print("file removed!")
+os.system("sh codec.sh")
